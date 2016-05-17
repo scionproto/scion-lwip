60c6602be0ff33cf7fc8a86fccf7d5ed3e75683f
pszalach <psz@inf.ethz.ch>
2016-05-17 09:15:40 +0200

    scion_addr added

---
+++ src/core/scion/scion_addr.c
@@ -0,0 +1,62 @@
+#include "lwip/ip_addr.h"
+
+void scion_addr(ip_addr_t *addr, u16_t isd, u32_t ad, u8_t type, u8_t *host_addr){
+    //TODO: add some sanity checks
+    addr->type = type;
+    *((u32_t *)addr->addr) = ISD_AS(isd, ad);
+    switch(type){
+        case ADDR_IPV4_TYPE:
+            memcpy(addr->addr + 4, host_addr, ADDR_IPV4_LEN);
+            break;
+        case ADDR_IPV6_TYPE:
+            memcpy(addr->addr + 4, host_addr, ADDR_IPV6_LEN);
+            break;
+        case ADDR_SVC_TYPE:
+            memcpy(addr->addr + 4, host_addr, ADDR_SVC_LEN);
+            break;
+        case ADDR_NONE_TYPE:
+            break;
+        default:
+            fprintf(stderr, "Wrong type in scion_addr()");
+    }
+}
+
+void scion_addr_set(ip_addr_t *dst, const ip_addr_t *src){
+    if (src == NULL)
+        bzero(dst->addr, MAX_ADDR_LEN);
+    else{
+        dst->type = src->type;
+        memcpy(dst->addr, src->addr, MAX_ADDR_LEN);
+    }
+}
+
+void ip_addr_set_any(ip_addr_t *addr){
+    bzero(addr->addr, MAX_ADDR_LEN);
+}
+
+u32_t ip4_addr_get_u32(const ip_addr_t *addr){
+    return *((u32_t *)(addr->addr + 4));
+}
+
+int ip_addr_cmp(const ip_addr_t *addr1, const ip_addr_t *addr2){
+    if (addr1 == NULL || addr2 == NULL)
+        return (addr1 == addr2);
+    if (bcmp(addr1->addr, addr2->addr, MAX_ADDR_LEN))
+        return 0;
+    return (addr1->type == addr2->type);
+}
+
+int scion_addr_isany(const ip_addr_t *addr){
+    u8_t zeros[MAX_ADDR_LEN] = {0};
+    if (addr == NULL)
+        return 1;
+    return !bcmp(addr->addr, zeros, MAX_ADDR_LEN);
+}
+
+
+void print_hex(char *buf, int len);
+void print_scion_addr(ip_addr_t *addr){
+    fprintf(stderr, "(%d,%d), type:%d ", ISD(*((u32_t *)(addr->addr))),
+            AS(*((u32_t *)(addr->addr))), addr->type);
+    print_hex(addr->addr+4, 16);
+}
